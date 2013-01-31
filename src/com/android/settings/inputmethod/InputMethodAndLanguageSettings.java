a7005050c4e4eb83b778a0beb23530f783044c46
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2013-01-31 12:04:57 +0900

    Add ability to hide volume rocker preferences
    also make the config_show_volumeRockerWake config to false
    
    see more details: http://review.cyanogenmod.org/#/c/26310/
    
    Change-Id: Id885aa423074b3e3fb871716560837a183357cac
    
    Conflicts:
    
    	res/values/config.xml
    	src/com/android/settings/DisplaySettings.java
    	src/com/android/settings/SoundSettings.java
    	src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java

--- src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
+++ src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
@@ -205,10 +205,14 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
 
         mVolumeKeyCursorControl = (ListPreference) findPreference(VOLUME_KEY_CURSOR_CONTROL);
         if(mVolumeKeyCursorControl != null) {
-            mVolumeKeyCursorControl.setOnPreferenceChangeListener(this);
-            mVolumeKeyCursorControl.setValue(Integer.toString(Settings.System.getInt(getActivity()
-                    .getContentResolver(), Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0)));
-            mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
+            if (Utils.hasVolumeRocker(getActivity())) {
+                mVolumeKeyCursorControl.setOnPreferenceChangeListener(this);
+                mVolumeKeyCursorControl.setValue(Integer.toString(Settings.System.getInt(getActivity()
+                        .getContentResolver(), Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0)));
+                mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
+            }
+        } else {
+            getPreferenceScreen().removePreference(mVolumeKeyCursorControl);
         }
 
         mDisableFullscreenKeyboard = (CheckBoxPreference) findPreference(PREF_DISABLE_FULLSCREEN_KEYBOARD);
